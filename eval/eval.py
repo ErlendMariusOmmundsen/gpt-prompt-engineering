6f8b70707fcf46fd3b2301a3ac11e935611c8e5f
ErlendMariusOmmundsen <emommundsen@gmail.com>
2023-03-22 16:14:29 +0100

    Add bert_score to eval

--- eval/eval.py
+++ eval/eval.py
@@ -2,17 +2,20 @@ from rouge_score import rouge_scorer
 from pyrouge import Rouge155
 import language_tool_python
 from nltk.tokenize import word_tokenize, sent_tokenize
+from bert_score import score
+import matplotlib.pyplot as plt
 
 
 class eval:
-    scorer = ""
+    scorer = rouge_scorer.RougeScorer(["rouge1", "rogue2", "rougeL"], use_stemmer=True)
+    langtool = language_tool_python.LanguageToolPublicAPI(
+        "en-US"
+    )  # use a remote server (automatically set up), language English
 
     def __init__(self):
-        self.scorer = rouge_scorer.RougeScorer(
-            ["rouge1", "rogue2", "rougeL"], use_stemmer=True
-        )
+        pass
 
-    def rogue(candidate, reference):
+    def rogue(self, candidate, reference):
         """
         Args:
           rouge_types: A list of rouge types to calculate.
@@ -40,3 +43,17 @@ class eval:
         for sent in sentences:
             total += self.error_count_score(sent)
         return total / len(sentences)
+
+    def bert_score(self, candidates, references):
+        # score inputs: list of candidate sentences, list of reference sentences
+        # score outputs: precision, recall, f1 tensors. Same number of elements as input
+
+        p, r, f1 = score(candidates, references, lang="en", verbose=True)
+
+        # get mean:
+        mean = f1.mean()
+
+        return p, r, f1, mean
+
+    def textual_entailment(self):
+        pass
