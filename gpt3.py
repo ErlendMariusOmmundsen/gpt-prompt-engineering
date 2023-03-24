fe3c0c0fc950376c4f6c12c73c31abb1cda342d8
ErlendMariusOmmundsen <emommundsen@gmail.com>
2023-03-24 19:34:36 +0100

    Add dataclasses

--- gpt3.py
+++ gpt3.py
@@ -1,3 +1,6 @@
+from typing import List
+from dataclasses import dataclass, fields, asdict
+from json import dumps
 import openai
 import configparser
 import pandas as pd
@@ -8,6 +11,68 @@ import tiktoken
 separator = "\n###\n"
 max_tokens = 4096
 
+@dataclass
+class Message:
+    role:str
+    content: str
+
+    @property
+    def __dict__(self):
+        return asdict(self)
+    
+    
+    @property
+    def json(self):
+        return dumps(self.__dict__)
+
+@dataclass
+class ChatChoice:
+    index: int
+    message: Message
+    finish_reason: str
+
+@dataclass
+class CompletionChoice:
+    text: str
+    index: int
+    logprobs: object
+    finish_reason: str
+
+@dataclass
+class Usage:
+    completion_tokens: int
+    prompt_tokens: int
+    total_tokens: int
+
+@dataclass
+class ChatResponse:
+    id: str
+    object: str
+    created: int
+    choices: List[ChatChoice]
+    usage: Usage
+
+@dataclass
+class CompletionResponse:
+    id: str
+    object: str
+    created: int
+    model: str
+    choices: List[CompletionChoice]
+    usage: Usage
+
+@dataclass
+class DfDict:
+    prompt_template: str
+    examples: List[str]
+    num_examples: int
+    text: str
+    prediction: str
+    finish_reason: str
+
+
+def serialize_messages(messages):
+    return [asdict(m) for m in messages]
 
 class gpt3:
     def __init__(self):
