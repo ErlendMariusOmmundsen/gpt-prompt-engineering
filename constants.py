ba2812aaf6787e1d4c4b13caa38cbf314acea3af
ErlendMariusOmmundsen <emommundsen@gmail.com>
2023-06-08 19:16:55 +0000

    Add templates

--- constants.py
+++ constants.py
@@ -15,12 +15,19 @@ BASELINE_TEMPLATE = Template(
     "suggest three insightful, concise subheadings which summarize this text, suggest three bullet points for each subheading:\n ${text}"
 )
 FOLLOW_UP_TEMPLATE = Template("Chat messages with follow_up_questions")
-IMPROVE_TEMPLATE = Template("Chat messages, ask to improve the output")
+IMPROVE_TEMPLATE = Template(
+    "Summarize the text[...] Text: ${text}. Here is a text and its summary. Please improve the summary.\n Text: ${text} \n Summary: ${summary}"
+)
+IMPORTANT_PARTS_TEMPLATE = Template(
+    "Find important parts of the text. Text: ${text}. Improve them.\n Summarize the text. Important parts: ${important_parts} Text: ${text}"
+)
 IN_CONTEXT_TEMPLATE = Template("Input: ${text} \nOutput:")
 INDUCE_TEMPLATE = Template(
     "${Context_setter} *sep* ${example_pairs} *sep* The instruction was:"
 )
-TOPIC_TEMPLATE = Template("${Context_setter} Topic: ${topic} Text: ${text} \nSummary:")
+TOPIC_TEMPLATE = Template(
+    "${Context_setter} The text is on the following topic(s): ${topic} Text: ${text}"
+)
 PERSONA_TEMPLATE = Template("${Context_setter} ${prompt} Text: ${text}\n")
 REPEAT_TEMPLATE = Template("${Prompt} Text: ${text} ${Prompt}")
 ZERO_SHOT_TEMPLATE = Template("${Prompt} Text: ${text} ")
